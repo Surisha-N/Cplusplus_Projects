c7e555d1ce718efc98d13ec363d49a898d661bed
Surisha Nadasen <nadasensurisha@gmail.com>
2025-07-02 11:42:27 +0200

    First Program

---
+++ Section_4_Getting_Started/FIrst_program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    int favoriteNumber;
+    cout<<"Enter your favorite number between 1 and 100:"<<endl;
+    cin>>favoriteNumber;
+    cout<< favoriteNumber << " is my favorite number too!" << endl;
+    return 0;
+}
